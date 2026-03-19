95f86e8ad0b1dcc550126548b035e5f8cbe4337b
flashgrey <aaftabkhan2005@gmail.com>
2026-03-19 16:52:54 +0530

    added code to run simulations

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from Simulator import Simulator
+
 def main():
-    print("Hello from autos!")
+    from algos.Evil import Evil
+    from algos.Nice import Nice
+    from algos.BadTitForTat import BadTitForTat
+    from algos.Dumb import Dumb
+    from algos.Forgiver import Forgiver
+    from algos.Grudger import Grudger
+    from algos.Perverse import Perverse
+    from algos.TitForTat import TitForTat
+
+    autos = [Evil(), Nice(), BadTitForTat(), Dumb(), Forgiver(), Grudger(), Perverse(), TitForTat()]
+
+    for bot_a in autos:
+        for bot_b in autos:
+            if bot_a.name == bot_b.name:
+                continue
+            sim = Simulator(bot_a=bot_a, bot_b=bot_b)
+
+            sim.run()
+
+            sim.results()
 
 
 if __name__ == "__main__":
